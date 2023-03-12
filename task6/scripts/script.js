dd14927b68b05ce54ecf82a4630ad25a328835a9
Murad Agamedov <82538242+Murad268@users.noreply.github.com>
2023-03-12 21:27:57 +0400

    Add files via upload

--- task6/scripts/script.js
+++ task6/scripts/script.js
@@ -11,6 +11,40 @@ function clickHamburger(trigger, menuClass, activeClass, menuActive) {
 clickHamburger(".navbar__hamburger", ".menu", 'navbar__hamburger__clicked', 'menu__active');
 
 
+
+
+
+$('.slider').slick({
+   infinite: true,
+   slidesToShow: 3,
+   slidesToScroll: 3,
+   dots: true,
+   prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><i class="fa fa-angle-left"></i></button>',
+   nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><i class="fa fa-angle-right"></i></button>',
+   responsive: [
+      {
+        breakpoint: 788,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      }
+    ]
+ });
+
+ $(".acordeon").on("click", ".acordeon-cabecera", function() {
+   $(this).toggleClass("active").next().slideToggle();
+   $(".acordeon-cabecera i").toggleClass("active__arr");
+   $(this).toggleClass("acc__title__active")
+ });
+
 // const sliderWidth = parseInt(document.querySelector('.carusel__slider').clientWidth);
 // const marginRight = parseInt(getComputedStyle(document.querySelector('.carusel__wrapper')).columnGap);
 // const innerr = document.querySelector('.carusel__inner').clientWidth;
